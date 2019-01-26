897dfb4d54a3ac443767b7fac25cbd7b9e2a43c6
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2019-01-26 15:59:39 +0200

    WIP

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-docker build -t $GL_IMAGE_NAME
-docker push $GL_IMAGE_NAME
+docker build -t $GITLAB_IMAGE_NAME .
+docker push $GITLAB_IMAGE_NAME
